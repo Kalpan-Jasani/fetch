370749a18b39ae696613ca1542c4216f999482cd
Eric Baeza <baeza.eric37@gmail.com>
2020-02-14 20:06:45 -0500

    adds personal boards creation for frontend using material ui.

---
+++ src/PersonalBoards.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import { Button, Checkbox, TextField, FormControlLabel } from '@material-ui/core';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core'
+
+class PersonalBoards extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            boardName: '',
+            isPrivate: false,
+            isDialogOpen: false,
+        }
+    }
+
+    handleInputChange = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+    }
+
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+
+        // get the form data out of state
+        const { boardName, isPrivate } = this.state;
+
+        // clear the form
+        this.setState({
+            boardName: '',
+            isPrivate: false,
+        });
+
+        console.log("Board Name: " + boardName)
+        console.log("Private: " + isPrivate)
+
+        // make the new board here
+
+
+    }
+
+    handleDialogOpen = () => {
+        this.setState({
+            isDialogOpen: true,
+        });
+    }
+
+    handleDialogClose = () => {
+        this.setState({
+            isDialogOpen: false,
+        });
+    }
+
+    render() {
+        return <div>
+            <h1>
+                Personal Boards
+            </h1>
+
+            <Button
+                variant="contained"
+                color="secondary"
+                onClick={this.handleDialogOpen}
+            >
+                New Personal Board
+            </Button>
+            <Dialog
+                open={this.state.isDialogOpen}
+                onClose={this.handleDialogClose}
+                aria-labelledby="form-dialog-title"
+            >
+                <DialogTitle id="form-dialog-title">
+                    Create a new Personal Board!
+            </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Please enter the name of your new Personal Board.
+                        If you make your personal board Private, no one but you will be able to see it!
+                </DialogContentText>
+
+                    <form
+                        onSubmit={this.handleSubmit}
+                        style={{ paddingLeft: 25, flexDirection: 'column', display: 'flex', paddingRight: 25, justifyContent: 'space-around', height: 250 }}
+                    >
+                        <TextField
+                            id="outlined-basic"
+                            label="Board Name"
+                            placeholder="Tech, Politics, etc..."
+                            value={this.state.boardName}
+                            name="boardName"
+                            onChange={this.handleInputChange}
+                            type="text"
+                            required
+                            color="secondary"
+                        />
+
+                        <FormControlLabel
+                            label="Private"
+                            control={
+                                <Checkbox
+                                    name="isPrivate"
+                                    checked={this.state.isPrivate}
+                                    onChange={this.handleInputChange}
+                                    color="secondary"
+                                />}
+                        />
+
+                        <Button variant="contained" color="secondary" type="submit">
+                            Create
+                </Button>
+                    </form>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.handleDialogClose} color="secondary">
+                        Cancel
+                    </Button>
+                </DialogActions>
+
+
+            </Dialog>
+
+
+
+        </div>
+    }
+
+}
+
+export default PersonalBoards;
+
