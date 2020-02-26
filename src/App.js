d4f6534ecccc1428230fed859d4a54cb57d1dbe1
Kalpan Jasani <kjasani@purdue.edu>
2020-02-26 09:12:51 -0500

    implement simple Sidebar

--- src/App.js
+++ src/App.js
@@ -1,46 +1,95 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { FirebaseAuthProvider, FirebaseAuthConsumer } from '@react-firebase/auth';
 
 import firebase from "firebase/app";
 import 'firebase/auth';
+
+import { makeStyles } from '@material-ui/core';
+
 
 import Login from './Login';
 import Home from './Home';
 import config from './config'
 import About from './About';
 import PersonalBoards from './PersonalBoards';
+import Sidebar from './Sidebar';
 
-class App extends React.Component {
-    render() {
-        return (
-            <FirebaseAuthProvider firebase={firebase} {...config}>
-                <FirebaseAuthConsumer>
-                    {({ isSignedIn, user, providerId }) =>
-                        isSignedIn ?
-                        <HashRouter>
-                            <Switch>
-                                <Route path="/about">
-                                    <About />
-                                </Route>
-                                <Route path="/home">
-                                    <Home />
-                                </Route>
-                                <Route path="/boards">
-                                    <PersonalBoards />
-                                </Route>
-                                <Route path="">
-                                    <Home />
-                                </Route>
-                            </Switch>
-                        </HashRouter>
-                        :
-                        <Login />
-                    }
-                </FirebaseAuthConsumer>
-            </FirebaseAuthProvider>
-        );
+const drawerWidth = 240;  // width of the sidebar (can change to adjust)
+
+
+const App = () => (
+    <FirebaseAuthProvider firebase={firebase} {...config}>
+        <FirebaseAuthConsumer> 
+            {({ isSignedIn, user, providerId }) =>
+            isSignedIn ?
+            <BasePage>
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/home">
+                        <Home />
+                    </Route>
+                    <Route path="/boards">
+                        <PersonalBoards />
+                    </Route>
+                    <Route path="">
+                        <Home />
+                    </Route>
+                </Switch>
+            </BasePage>
+            :
+            <Login />
+        }
+        </FirebaseAuthConsumer>
+    </FirebaseAuthProvider>
+);
+
+
+// taken from material-ui drawers
+const useStyles = makeStyles(theme => ({
+    root: {
+      display: 'flex',
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(2),
+    },
+    avatar: {
+        backgroundColor: "#aaa",
+        height: "6rem",
+        width: "6rem",
     }
+}));
+
+
+/**
+ * 
+ * @param {*} props: props contains children to display actual app content 
+ *  (non sidebar content)
+ * 
+ * This is the component which acts like a wrapper component to allow 
+ *  a sidebar to exist
+ */
+function BasePage(props) {
+    console.log('flag 1')
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <Sidebar classes={classes}/>
+            <div className={classes.content}>
+                {props.children}
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Drawer, Divider, Button, Avatar } from "@material-ui/core";
+import HomeIcon from '@material-ui/icons/Home';
+import AddCircleOutlinedIcon from '@material-ui/icons/AddCircleOutlined';
+import { Link } from 'react-router-dom';
+
+import './sidebar.css';
+
+function Sidebar(props) {
+
+    return (
+        <Drawer
+            variant="permanent"
+            anchor="left"
+            open={true}
+            className={props.classes.drawer}
+            classes={{
+                paper: props.classes.drawerPaper,
+            }}
+        >
+            <div className="sidebar-item">     
+                <Avatar className={props.classes.avatar}>AB</Avatar>
+            </div>
+            <Link to="/home" className="sidebar-item home link">
+                <HomeIcon/>
+                <span>Home</span>
+            </Link>
+            <Divider/>
+            <Link to="/starred" className="sidebar-item starred link">
+                Starred articles
+            </Link>
+            <Link to="/boards" className="sidebar-item personal-boards link">
+                Boards
+            </Link>
+            <Link to="/community-boards" className="sidebar-item community-boards link">
+                Community Boards
+            </Link>
+            <Divider/>
+            <Button id="add-article-sidebar">
+                <AddCircleOutlinedIcon/>
+                New article
+            </Button>
+            
+        </Drawer>
+    )
+}
+
+export default Sidebar;
