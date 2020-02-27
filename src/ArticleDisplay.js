d2877a3a7ef13e541b28903b1be6ed18fb0be277
Seerat Dheer <sdheer@purdue.edu>
2020-02-27 02:16:21 -0500

    Starring Functionality

--- src/ArticleDisplay.js
+++ src/ArticleDisplay.js
@@ -4,19 +4,42 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Favorite from '@material-ui/icons/Favorite';
-import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import Star from '@material-ui/icons/Star';
+import StarBorder from '@material-ui/icons/StarBorder';
+import firebase from "firebase";
 
 class ArticleDisplay extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-           url: ''
+           url: '',
+           isStarred: false,
         }
     }
 
-    render() {
+    handleStar = async (event) => {
 
+
+        const target = event.target;
+        console.log("Star Value: " + target.checked);
+        const isStarred = !this.state.isStarred;
+
+        this.setState({
+                isStarred: isStarred
+        });
+
+       await firebase.firestore().collection("localArticles")
+        .doc("users")
+        .collection("ZoiGTzwfFugLUTUP9s6JbcpHH3C2") // hardcoded userid
+        .doc("2nhPDNTTDFoYGgmFO4aD")
+        .update({
+            starred: target.checked
+        });
+        
+    }
+
+    render() {
+        
         return (
         <Card style={{maxWidth: 600, minHeight: 300 , margin: 25 }} >
             <CardContent>
@@ -24,10 +47,10 @@ class ArticleDisplay extends React.Component {
             </CardContent>
             <CardActions style={{ paddingLeft: 20 }}>
                 <FormControlLabel
-                    control={<Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} value="checkedH" />}
-                    label="Like"
+                    control={<Checkbox icon={<StarBorder />} checkedIcon={<Star />} checked={this.state.isStarred} onClick={this.handleStar} />}
+                    label="Star"
                 />
-            </CardActions>
+            </ CardActions>
         </Card>
         );
   }
