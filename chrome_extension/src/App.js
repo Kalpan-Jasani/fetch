349a764d7972529253deadd16979bb76af5f0289
Kalpan Jasani <kjasani@purdue.edu>
2020-03-21 01:13:53 -0400

    allow login in the extension

--- chrome_extension/src/App.js
+++ chrome_extension/src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import firebaseConfig from './config';
+
+import firebase from "firebase/app";
+import "firebase/auth";
+import { FirebaseAuthProvider, FirebaseAuthConsumer } from '@react-firebase/auth';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
+                <FirebaseAuthConsumer>
+                    {({ isSignedIn, user, providerId }) =>
+                    isSignedIn ?
+                    <p>Hello {firebase.auth().currentUser.displayName} </p>
+                    :
+                    <Login/>
+                    }
+                </FirebaseAuthConsumer>
+            </FirebaseAuthProvider>
+            <Logout/>
+        </div>
+    );
 }
 
+const handleGoogleSignIn = async (event) => {
+    var googleProvider = new firebase.auth.GoogleAuthProvider();
+    var result = await firebase.auth().signInWithPopup(googleProvider).catch((error) => {
+        console.error(error.code);
+    });
+
+    console.log("flag 1");
+    console.log(result);
+    console.log("new? " + String(result.additionalUserInfo.isNewUser));
+    if (result === undefined) {
+        return;
+    }
+}
+
+function Login() {
+    return (
+        <button onClick={handleGoogleSignIn}>Sign in with Google (for now)</button>
+    )
+}
+
+function Logout() {
+    return (
+        <button onClick={() => firebase.auth().signOut()}>Logout</button>
+    );
+}
 export default App;
