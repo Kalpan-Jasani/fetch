16a4dc1a10078da70808c4ecc14ca6ae467bd938
Seerat Dheer <sdheer@purdue.edu>
2020-04-29 02:36:44 -0400

    Styling changes on the modal

--- web_app/src/PlayQueue.js
+++ web_app/src/PlayQueue.js
@@ -17,7 +17,9 @@ import { blue } from '@material-ui/core/colors';
 const styles = (theme) => ({
   root: {
     display: 'block',
-    height: '100%'
+    height: '100%',
+    borderRadius: 7,
+  //  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
   },
   row: {
     display: 'table',
