342fa6cd7715d73bb136899bf49022a7e7f16378
Seerat Dheer <sdheer@purdue.edu>
2020-04-30 03:42:10 -0400

    Starred items and side bar styling

--- web_app/src/StarredItems.js
+++ web_app/src/StarredItems.js
@@ -2,16 +2,7 @@ import React from 'react';
 import firebase from "firebase";
 import StarredArticleDisplay from './StarredArticleDisplay';
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-
-const styles = {
-    titleBar: {
-        borderRadius: 3,
-        boxShadow: '0 3px 5px 2px rgba(0, 0, 0, 0.5)',
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    }
-  };
+import { Divider } from '@material-ui/core';
 
 class StarredItems extends React.Component {
     
@@ -58,14 +49,11 @@ class StarredItems extends React.Component {
     }
 
     render() {
-        const { classes } = this.props;
         return (
             <div style={{display: 'inline-block', margin: '20px'}}>
-                <AppBar  position="static" style={{background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)', width: '400px'}}>
-                    <Toolbar variant="dense">
-                    <h3> Starred Articles ({this.state.articles.length})</h3>
-                    </Toolbar>
-                </AppBar>
+                    <h2> Starred Articles ({this.state.articles.length})</h2>
+                    <Divider />
+                
                { this.state.articles.map((article) => {
                   return (
                 <div className={article.read ? "article-read": "article-unread"} style={{ margin: '20px'}} >
@@ -89,4 +77,4 @@ class StarredItems extends React.Component {
 
 }
 
-export default withStyles(styles) (StarredItems);
+export default StarredItems;
